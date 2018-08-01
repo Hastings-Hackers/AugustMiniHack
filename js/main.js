ecc996793254e164350973568c01928b947c6b0e
Gareth Marland <gareth.marland@gmail.com>
2018-08-01 18:24:54 +0100

    adding analyser

--- js/main.js
+++ js/main.js
@@ -51,40 +51,33 @@ window.addEventListener( 'resize', function() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }, false);
 
-try
-{
+try {
 	// Create a instance of AudioContext interface
 	window.AudioContext = window.AudioContext||window.webkitAudioContext||window.mozAudioContext;
+
 	var context = new AudioContext();
 
-	var source = context.createBufferSource(); //this represents the audio source. We need to now populate it with binary data.
+	var analyser = context.createAnalyser();
+	analyser.fftSize = 2048;
 
-	//now retrieve some binary audio data from <audio>, ajax, input file or microphone and put it into a audio source object.
-	//here we will retrieve audio binary data via AJAX
+
+	var source = context.createBufferSource(); 
+
 	var request = new XMLHttpRequest();
 	request.open('GET', 'http://localhost:8080/music/test.mp3', true);
-	request.responseType = 'arraybuffer'; //This asks the browser to populate the retrieved binary data in a array buffer
+	request.responseType = 'arraybuffer';
 	request.onload = function(){
-		//populate audio source from the retrieved binary data. This can be done using decodeAudioData function.
-		//first parameter of decodeAudioData needs to be array buffer type. So from wherever you retrieve binary data make sure you get in form of array buffer type.
 		context.decodeAudioData(request.response, function(buffer) {
-				source.buffer = buffer;
+			console.log(buffer);
+			source.buffer = buffer;
 		}, null);
 	}
 	request.send();
 
-	//now we got context, audio source.
-	//now lets connect the audio source to a destination(hardware to play sound).
-	source.connect(context.destination);//destination property is reference the default audio device
+	source.connect(context.destination);
 
-	/*
-		If we wanted to add any audio nodes then we need to add them in between audio source and destionation anytime dynamically.
-	*/
-
-	//now play the sound.
 	source.start(0);
-	}
-catch(e) 
-{
+}
+catch(e)  {
 	alert("Web Audio API not supported");
 }
